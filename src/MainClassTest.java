ee10a52ca0bafaf1c666dcafbf7dc49457033269
Anastasija <xitrik@list.ru>
2021-09-08 22:16:40 +0300

    Initial commit

---
+++ src/MainClassTest.java
@@ -0,0 +1,13 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainClassTest {
+
+  @Test
+  public void testGetLocalNumber(){
+    MainClass number = new MainClass();
+    int result = number.getLocalNumber();
+    int expected = 14;
+    Assert.assertTrue("Got wrong number. Expected: "+expected+", received: "+ result, result==expected );
+  }
+}
