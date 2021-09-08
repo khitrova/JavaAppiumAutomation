295fa0734a9e01c2ef1f8f798c84939b4a1f3a3c
Anastasija <xitrik@list.ru>
2021-09-08 22:30:19 +0300

    Homework Test 1

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -5,9 +5,11 @@ public class MainClassTest {
 
   @Test
   public void testGetLocalNumber(){
+
     MainClass number = new MainClass();
     int result = number.getLocalNumber();
     int expected = 14;
+
     Assert.assertTrue("Got wrong number. Expected: "+expected+", received: "+ result, result==expected );
   }
 }
